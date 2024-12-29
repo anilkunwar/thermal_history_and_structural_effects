ed44084998ebd91050f6ddfeff394f2d2616fa22
Anil Kunwar <anilkunwar318@yahoo.com>
2024-12-29 14:53:47 +0100

    Update gaussian_heat_source.py

--- gaussian_heat_source.py
+++ gaussian_heat_source.py
@@ -66,13 +66,7 @@ fig1.update_layout(scene=dict(
                         xaxis=dict(title_font=dict(size=20)),  # Adjust X-axis font size
                         yaxis=dict(title_font=dict(size=20)),  # Adjust Y-axis font size
                         zaxis=dict(title_font=dict(size=20), range=[0, max_intensity]),   # Adjust Z-axis font size and range
-                        #title={'text': "Power = 250 W",          # Title text
-                        #       'x': 0.5,                           # Horizontal alignment (0=left, 1=right, 0.5=center)
-                        #       'xanchor': 'center',                # Anchoring relative to `x`
-                        #       'y': 0.9,                           # Vertical position of the title
-                        #       'yanchor': 'top'                    # Anchoring relative to `y`
-                        #       },
-                        #title_font=dict(size=20, color='blue'), # Customize title font size and color
+                        title=dict(text="P = 250 W", font=dict(size=50), automargin=True, yref='paper')
                     ),
                   width=800, height=600)
 fig1.update_coloraxes(colorbar=dict(
@@ -93,13 +87,7 @@ fig2.update_layout(scene=dict(
                         xaxis=dict(title_font=dict(size=20)),  # Adjust X-axis font size
                         yaxis=dict(title_font=dict(size=20)),  # Adjust Y-axis font size
                         zaxis=dict(title_font=dict(size=20), range=[0, max_intensity]),   # Adjust Z-axis font size and range
-                        #title={'text': "Power = 350 W",          # Title text
-                        #       'x': 0.5,                           # Horizontal alignment (0=left, 1=right, 0.5=center)
-                        #       'xanchor': 'center',                # Anchoring relative to `x`
-                        #       'y': 0.9,                           # Vertical position of the title
-                        #       'yanchor': 'top'                    # Anchoring relative to `y`
-                        #       },
-                        #title_font=dict(size=20, color='blue'), # Customize title font size and color
+                        title=dict(text="P = 350 W", font=dict(size=50), automargin=True, yref='paper')
                     ),
                   width=800, height=600)
 fig2.update_coloraxes(colorbar=dict(
