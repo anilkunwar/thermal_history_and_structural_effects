b420fa3a1be2322119d6e786ed799231f5fa2669
Anil Kunwar <anilkunwar318@yahoo.com>
2024-12-29 14:57:40 +0100

    Update gaussian_heat_source.py

--- gaussian_heat_source.py
+++ gaussian_heat_source.py
@@ -66,8 +66,9 @@ fig1.update_layout(scene=dict(
                         xaxis=dict(title_font=dict(size=20)),  # Adjust X-axis font size
                         yaxis=dict(title_font=dict(size=20)),  # Adjust Y-axis font size
                         zaxis=dict(title_font=dict(size=20), range=[0, max_intensity]),   # Adjust Z-axis font size and range
-                        title=dict(text="P = 250 W", font=dict(size=50), automargin=True, yref='paper'),
+                        
                     ),
+                  title=dict(text="P = 250 W", font=dict(size=50), automargin=True, yref='paper'),
                   width=800, height=600)
 fig1.update_coloraxes(colorbar=dict(
                         exponentformat='none',     # Prevents exponent format
@@ -87,8 +88,9 @@ fig2.update_layout(scene=dict(
                         xaxis=dict(title_font=dict(size=20)),  # Adjust X-axis font size
                         yaxis=dict(title_font=dict(size=20)),  # Adjust Y-axis font size
                         zaxis=dict(title_font=dict(size=20), range=[0, max_intensity]),   # Adjust Z-axis font size and range
-                        title=dict(text="P = 350 W", font=dict(size=50), automargin=True, yref='paper'),
+                        #title=dict(text="P = 350 W", font=dict(size=50), automargin=True, yref='paper'),
                     ),
+                  title=dict(text="P = 350 W", font=dict(size=50), automargin=True, yref='paper'),
                   width=800, height=600)
 fig2.update_coloraxes(colorbar=dict(
                         exponentformat='none',     # Prevents exponent format
